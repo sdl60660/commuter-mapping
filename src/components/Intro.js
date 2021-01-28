3608ff163dc887483e926f7aa7d92b5b3da670b8
sdl60660 <learnersd@gmail.com>
2021-01-28 17:33:54 -0500

    Fixed a typo and made a small stying change.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ const Intro = (props) => {
     return (
         <div className={"intro-container"}>
             <div className={"intro-container__row"} id={"title-row"}>
-                <h1 className="intro-container__title">Defining Metro Area Commuter Pattners</h1>
+                <h1 className="intro-container__title">Defining Metro Area Commuter Patterns</h1>
             </div>
             <div className={"intro-container__row"}>
                 This is a draft project. It uses LODES (LEHD Origin-Destination Employment Statsitics) data from the U.S. Census to better understand and visualize 
