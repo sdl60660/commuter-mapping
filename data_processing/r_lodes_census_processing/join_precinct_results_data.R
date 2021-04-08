b7f84b0ab261f6016635f56fd48bc912fb8746f1
sdl60660 <learnersd@gmail.com>
2021-04-08 11:37:58 -0400

    Made title header larger

--- data_processing/r_lodes_census_processing/join_precinct_results_data.R
+++ data_processing/r_lodes_census_processing/join_precinct_results_data.R
@@ -19,7 +19,17 @@ msa_city_dict <- read_csv('../data/msa_largest_cities.csv') %>%
 places <- st_read('../data/places.geojson') %>%
   filter(GEOID %in% msa_city_dict$CITY_ID)
 city_centroids <- st_centroid(places)
-
+# Load overall commuter rates for MSAs
+msa_commuter_rates <- read_csv("../data/metro_commuter_totals.csv") %>%
+  filter(is.na(in_center_city)) %>%
+  select(-c(MSA, largest_city, in_center_city))
+# Append prefix to column names
+colnames(msa_commuter_rates) <- paste("MSA", colnames(msa_commuter_rates), sep="_")
+# Remove prefix from first column (MSA_ID) and cast it as character type
+msa_commuter_rates <- msa_commuter_rates %>%
+  rename(MSA_ID=MSA_MSA_ID) %>%
+  mutate(MSA_ID=as.character(MSA_ID))
+  
 
 # Find the precinct centroids and spatial join them to census tracts
 precinct_centroids <- st_centroid(precinct_geos)
@@ -54,15 +64,49 @@ filtered_tract_data <- full_tract_data %>%
     is.na(commuter_pct) == FALSE,
     is.na(dem_lead) == FALSE
   ) %>%
-  left_join(msa_city_dict, by="MSA_ID")
+  left_join(msa_city_dict, by="MSA_ID") %>%
+  rename(
+    MSA_LARGEST_CITY=CITY.y,
+    MSA_LARGEST_CITY_ID=CITY_ID.y,
+    TRACT_CITY=CITY.x,
+    TRACT_CITY_ID=CITY_ID.x,
+    MSA=MSA.x,
+    STATE_ID=STATE_ID.x
+  ) %>%
+  select(
+    -c(MSA.y, STATE_ID.y)
+  )
 
+tract_data_with_MSA_totals <- filtered_tract_data %>%
+  left_join(msa_commuter_rates, by="MSA_ID") %>%
+  mutate(adjusted_commuter_rate = commuter_pct - MSA_city_commuter_rate_2018)
 
-cor(filtered_tract_data$commuter_pct, filtered_tract_data$dem_lead)
-ggplot(filtered_tract_data, aes(x=commuter_pct, y=dem_lead)) + geom_point(size = 0.4, alpha=0.4) + geom_smooth(method=lm)
+# Get tract census data to determine population density
+us_states <- unique(fips_codes$state)[1:51]
+census_vars <- load_variables(2018, dataset="acs5", cache=T)
+us_tracts_pop <- map_df(us_states, function(us_state) {
+  get_acs(state = us_state, geography="tract", variables="B00001_001", year=2018)
+})
+us_tracts_geo <- map_df(us_states, function(us_state) {
+  tracts(state = us_state, cb = TRUE, year = 2018)
+})
+us_tracts <- us_tracts_pop %>%
+  left_join(us_tracts_geo, by="GEOID") %>%
+  select(GEOID, estimate, ALAND) %>%
+  rename(population = estimate) %>%
+  mutate(tract_population_density = population / ALAND)
 
-mutated_df <- filtered_tract_data %>%
+# Attach population density numbers to full tract df for comparison between commuter rate and tract density
+final_tract_data_df <- tract_data_with_MSA_totals %>%
+  left_join(us_tracts, by="GEOID") %>%
+  filter(!is.na(tract_population_density))
+
+cor(final_tract_data_df$tract_population_density, final_tract_data_df$dem_lead)
+ggplot(final_tract_data_df, aes(x=tract_population_density, y=dem_lead)) + geom_point(size = 0.4, alpha=0.4) + geom_smooth(method=lm)
+
+mutated_df <- final_tract_data_df %>%
   mutate(
-    rounded_commuter_pct = (round(commuter_pct*20, digits=0))/20
+    rounded_commuter_pct = (round(adjusted_commuter_rate*10, digits=0))/10
   )
 
 grouped_tracts <- mutated_df %>%
